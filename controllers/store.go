dcbd09962e2a911b0768a6a41c2375e661940e72
anandafiqri <anandafiqri1403@gmail.com>
2023-02-18 22:02:14 +0700

    success create data store

---
+++ controllers/store.go
@@ -0,0 +1,57 @@
+package controllers
+
+import (
+	"net/http"
+	"strconv"
+
+	l "github.com/forumGamers/store-service/loaders"
+	m "github.com/forumGamers/store-service/models"
+	"github.com/gin-gonic/gin"
+	"github.com/jinzhu/gorm"
+)
+
+func getDb() *gorm.DB {
+	return l.GetDb()
+}
+
+
+func CreateStore(c *gin.Context){
+	var store m.Store
+
+	name,image,description,owner_id := c.PostForm("name"),c.PostForm("image"),c.PostForm("description"),c.PostForm("owner_id")
+
+	if name == ""  || owner_id == "" {
+		c.AbortWithStatusJSON(http.StatusBadRequest,gin.H{"message" : "invalid data"})
+		return
+	}
+
+	store.Name = name
+
+	store.Image = image
+
+	store.Description = description
+
+	id,_ := strconv.ParseInt(owner_id,10,64)
+
+	store.Owner_id = int(id)
+
+	err := make(chan bool)
+
+	go func () {
+		res := getDb().Create(&store)
+
+		if res.Error != nil {
+			err <- true
+		}else {
+			err <- false
+		}
+	}()
+
+	if <- err == false {
+		c.JSON(http.StatusCreated,gin.H{"message":"success"})
+		return
+	}else {
+		c.JSON(http.StatusInternalServerError,gin.H{"message" : "Internal Server Error"})
+		return
+	}
+}
