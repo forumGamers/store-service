0632a6dadc290355c9b3effce18dceaffd31b3a2
anandafiqri <anandafiqri1403@gmail.com>
2023-03-05 20:57:42 +0700

    get favorite list

--- controllers/favorite.go
+++ controllers/favorite.go
@@ -65,3 +65,72 @@ func AddFavorite(c *gin.Context){
 
 	c.JSON(http.StatusOK,gin.H{"message":"success"})
 }
+
+func GetMyFavorite(c *gin.Context){
+	user := c.Request.Header.Get("id")
+	limit,page := 
+	c.Query("limit"),
+	c.Query("page")
+
+	var lmt int
+	var pg int
+
+	id,err := strconv.ParseInt(user,10,64)
+
+	if err != nil {
+		panic("Forbidden")
+	}
+
+	if limit == "" {
+		lmt = 10
+	}else {
+		l,r := strconv.ParseInt(limit,10,64)
+
+		if r != nil {
+			panic("Invalid params")
+		}
+
+		lmt = int(l)
+	}
+
+	if page == "" {
+		pg = 1
+	}else {
+		p,r := strconv.ParseInt(page,10,64)
+
+		if r != nil {
+			panic("Invalid params")
+		}
+
+		pg = int(p)
+	}
+
+	errCh := make(chan error)
+	dataCh := make(chan []m.Favorite)
+
+	go func(userId int,page int,limit int){
+		var data []m.Favorite
+		if err := getDb().Model(m.Favorite{}).Where("user_id = ?",userId).Offset((page - 1) * limit).Limit(limit).Find(&data).Error ; err != nil {
+			if err == gorm.ErrRecordNotFound {
+				errCh <- errors.New("Data not found")
+				dataCh <- nil
+				return
+			}else {
+				errCh <- err
+				dataCh <- nil
+				return
+			}
+		}
+
+		errCh <- nil
+		dataCh <- data
+	}(int(id),int(pg),int(lmt))
+
+	if err := <- errCh ; err != nil {
+		panic(err.Error())
+	}
+
+	favorite := <- dataCh
+
+	c.JSON(http.StatusOK,favorite)
+}
--- routes/favorite.go
+++ routes/favorite.go
@@ -9,5 +9,7 @@ func (r routes) favoriteRoutes(rg *gin.RouterGroup){
 
 	uri := rg.Group("/favorite")
 
+	uri.GET("/",c.GetMyFavorite)
+
 	uri.POST("/:itemId",c.AddFavorite)
 }
