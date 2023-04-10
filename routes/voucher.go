50ef982288184f4e35bbdb6186a5d67fbcc72a69
anandafiqri <anandafiqri1403@gmail.com>
2023-04-10 14:56:43 +0700

    move voucher to cmd

--- routes/voucher.go
+++ routes/voucher.go
@@ -1,6 +1,7 @@
 package routes
 
 import (
+	"github.com/forumGamers/store-service/cmd"
 	c "github.com/forumGamers/store-service/controllers"
 	"github.com/gin-gonic/gin"
 )
@@ -8,11 +9,11 @@ import (
 func (r routes) voucherRoutes(rg *gin.RouterGroup){
 	uri := rg.Group("/voucher")
 
-	uri.POST("/",c.AddVoucher)
+	uri.POST("/",cmd.AddVoucher)
 
 	uri.GET("/",c.GetAllVoucher)
 
 	uri.GET("/:id",c.GetVoucherById)
 
-	uri.DELETE("/:id",c.DeleteVoucher)
+	uri.DELETE("/:id",cmd.DeleteVoucher)
 }
