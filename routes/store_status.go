4be690536b4cab0090ca008a1a4a04b66a01db11
anandafiqri <anandafiqri1403@gmail.com>
2023-04-10 14:21:14 +0700

    move store_status to cmd

--- routes/store_status.go
+++ routes/store_status.go
@@ -1,6 +1,7 @@
 package routes
 
 import (
+	"github.com/forumGamers/store-service/cmd"
 	c "github.com/forumGamers/store-service/controllers"
 	md "github.com/forumGamers/store-service/middlewares"
 	"github.com/gin-gonic/gin"
@@ -10,11 +11,11 @@ func (r routes) store_status_routes(rg *gin.RouterGroup ){
 
 	uri := rg.Group("/store_status")
 
-	uri.POST("/",md.AuthorizeAdmin,c.CreateStoreStatus)
+	uri.POST("/",md.AuthorizeAdmin,cmd.CreateStoreStatus)
 
 	uri.GET("/",c.GetAllStoreStatus)
 
-	uri.PATCH("/change-name/:id",md.AuthorizeAdmin,c.UpdateStoreStatusName)
+	uri.PATCH("/change-name/:id",md.AuthorizeAdmin,cmd.UpdateStoreStatusName)
 
-	uri.PATCH("/change-exp/:id",md.AuthorizeAdmin,c.UpdateStoreStatusExp)
+	uri.PATCH("/change-exp/:id",md.AuthorizeAdmin,cmd.UpdateStoreStatusExp)
 }
