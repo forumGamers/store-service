257d608e344d7ee5fbb67a82a4385dcac8127c73
anandafiqri <anandafiqri1403@gmail.com>
2023-04-10 14:00:14 +0700

    add authentication

--- go.mod
+++ go.mod
@@ -7,6 +7,7 @@ require (
 	github.com/gin-contrib/cors v1.4.0
 	github.com/gin-contrib/logger v0.2.5
 	github.com/gin-gonic/gin v1.8.2
+	github.com/golang-jwt/jwt/v4 v4.5.0
 	github.com/jinzhu/gorm v1.9.16
 	github.com/joho/godotenv v1.5.1
 	github.com/lib/pq v1.10.7
---
+++ middlewares/authentication.go
@@ -0,0 +1,50 @@
+package middlewares
+
+import (
+	"errors"
+	"os"
+
+	"github.com/gin-gonic/gin"
+	"github.com/golang-jwt/jwt/v4"
+	"github.com/joho/godotenv"
+)
+
+func getSecretKey() (string,error){
+	if err := godotenv.Load() ; err != nil {
+		return "",errors.New("Failed to load env")
+	}
+
+	return os.Getenv("SECRET"),nil
+}
+
+func Authentication(c *gin.Context){
+	access_token := c.Request.Header.Get("access_token")
+
+	if access_token == "" {
+		panic("Invalid token")
+	}
+
+	secret,err := getSecretKey()
+
+	if err != nil {
+		panic(err.Error())
+	}
+
+	claim := jwt.MapClaims{}
+
+	token,err := jwt.ParseWithClaims(access_token,claim,func(t *jwt.Token)(interface{},error){
+		return []byte(secret),nil
+	})
+
+	if err != nil {
+		panic("signature is invalid")
+	}
+
+	if !token.Valid {
+		panic("Invalid token")
+	}
+
+	c.Set("user",claim)
+
+	c.Next()
+}
