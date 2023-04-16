a0c6ada7dfdc6ae38c3adb1d7ca561961587f5be
anandafiqri <anandafiqri1403@gmail.com>
2023-04-16 13:42:47 +0700

    make helper function getUser

---
+++ helper/global.go
@@ -0,0 +1,32 @@
+package helper
+
+import (
+	m "github.com/forumGamers/store-service/models"
+	"github.com/gin-gonic/gin"
+	"github.com/golang-jwt/jwt/v4"
+)
+
+func GetUser(c *gin.Context) m.User {
+	claim := c.MustGet("user").(jwt.MapClaims)
+	var user m.User
+
+	for key, val := range claim {
+		switch key {
+		case "email":
+			user.Email = val.(string)
+		case "fullName":
+			user.Fullname = val.(string)
+		case "iat":
+			user.Iat = int(val.(float64))
+		case "id":
+			user.Id = int(val.(float64))
+		case "isVerified":
+			user.IsVerified = val.(bool)
+		case "phoneNumber":
+			user.PhoneNumber = val.(string)
+		case "username":
+			user.Username = val.(string)
+		}
+	}
+	return user
+}
